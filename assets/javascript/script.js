ee8baae5c55519d6b581ff4c905a0b70f55af160
Chris Goslin <cgoslin7@gmail.com>
2021-03-08 17:06:55 -0600

    working on javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,52 @@
 
-const settingsWeather = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://us-weather-by-city.p.rapidapi.com/getweather?city=San%20Francisco&state=CA",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "cc3bdfe5d0mshc16b581a0701a7ep103683jsn6aabd3b9dcb2",
-		"x-rapidapi-host": "us-weather-by-city.p.rapidapi.com"
-	}
-};
+document.getElementById("search").onclick = function() {myFunction()};
 
-$.ajax(settingsWeather).done(function (response) {
-	console.log(response);
-});
+function myFunction() {
+    var cityName = document.getElementById("card-input").value
+    var apikey = "ebc3f1f4905a6bd26e6d5d0ef96dcd5d"
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`
+
+
+fetch(url)
+    .then(function(response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json()
+    }).then(function (response) {
+        console.log(response)
+
+        for (var i=0; i< response.daily[i].length; i++) {
+            response
+        }
+    })
+
+}
+
+
+
+
+
+
+        // current weather promise
+
+        // var url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apikey}`
+        // fetch(url2)
+        //     .then(function (response) {
+        //         if (!response.ok) {
+        //             throw response.json();                  
+        //         }
+        //         return response.json();
+        //     }).then(function (response) {
+        //         console.log(response)
+            
+        //         for(var i =0; i< response.daily[i].length; i++){
+        //             response
+        //         }
+
+        //         //forecast data
+        //     })
+    
+            
 
 
