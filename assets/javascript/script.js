ac40916b37fa88ead9e9f59ec8591ec8e1e2c513
Chris Goslin <cgoslin7@gmail.com>
2021-03-08 20:36:53 -0600

    placeholder added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,9 @@
 
 document.getElementById("search").onclick = function() {myFunction()};
+var saveCity = localStorage.getItem("city");
+console.log(saveCity);
+document.getElementById("card-input").placeholder = saveCity;
+
 
 var curday = function(sp){
     today = new Date();
@@ -28,22 +32,19 @@ fetch(url)
     }).then(function (response) {
         console.log(response)
 
-       
-    
-
-    localStorage.setItem("city",document.getElementById("card-input").value);
+     localStorage.setItem("city",document.getElementById("card-input").value);
 
     var cityEl = response.name;
-    $("#city-header").html(cityEl) + curday;
+    $("#city-header").html(cityEl + "  " + curday("/")); 
 
     var currentTempEl = response.main.temp;
-    $("#list-group-city1").html("Current Temperature:" + currentTempEl + "&deg;k");
+    $("#list-group-city1").html("Current Temperature:" + " " + currentTempEl + "&deg;k");
 
     var humidityEl = response.main.humidity;
-    $("#list-group-city2").html("Current Humidity:" + humidityEl + "%");
+    $("#list-group-city2").html("Current Humidity:" + " " + humidityEl + "%");
 
     var windEl = response.wind.speed;
-    $("#list-group-city3").html("Current Wind Speed:" + windEl + "MPH");
+    $("#list-group-city3").html("Current Wind Speed:" + " " + windEl + "MPH");
 
     // var uvEl = response.;
     // $("#list-group-city3").html("Current UV index:" + uvEl)
